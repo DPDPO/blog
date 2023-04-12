9ad1ef3b3336104b275065c2363d222eb5611d36
psc <whirled7@gmail.com>
2023-04-12 12:21:15 +0900

    style

--- pages/posts/first-post.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FirstPost = () => {
-  return <div>first-post</div>;
-};
-
-export default FirstPost;
--- pages/posts/second-post.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SecondPost = () => {
-  return <div>second-post</div>;
-};
-
-export default SecondPost;
