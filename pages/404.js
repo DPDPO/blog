d6ef347ec67d2529f35eb3f8a74cb8bbd3d3e7c8
psc <whirled7@gmail.com>
2023-04-22 07:44:15 +0900

    404

---
+++ pages/404.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import React from "react";
+
+const Custom404 = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "80vh",
+      }}
+    >
+      <Image
+        priority
+        src="/images/profile.png"
+        height={144}
+        width={144}
+        alt=""
+      />
+      <div>찾을 수 없는 페이지 입니다.</div>
+    </div>
+  );
+};
+
+export default Custom404;
