babe8fdd24fe050537ef38ae6fdeaab0db960bf8
psc <whirled7@gmail.com>
2023-05-02 08:20:51 +0900

    fix : 404 page

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
 import Layout from "@/components/Layout";
 import "../styles/globals.css";
 import { useRouter } from "next/router";
+import Custom404 from "./404";
 
 const App = ({ Component, pageProps }) => {
   const router = useRouter();
 
   return (
-    <Layout home={router.pathname === "/"}>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {router.pathname === "/404" ? (
+        <Custom404 statusCode={"404"} />
+      ) : (
+        <Layout home={router.pathname === "/"}>
+          <Component {...pageProps} />
+        </Layout>
+      )}
+    </>
   );
 };
 
