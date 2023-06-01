576fc73c342eba562635e27c42bad5bca54e704b
psc <whirled7@gmail.com>
2023-06-02 08:22:55 +0900

    style: 404

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,26 @@
-import Image from "next/image";
+// import Image from "next/image";
 import React from "react";
 
 const Custom404 = () => {
   return (
+    // <div
+    //   style={{
+    //     display: "flex",
+    //     flexDirection: "column",
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //     height: "80vh",
+    //   }}
+    // >
+    //   <Image
+    //     priority
+    //     src="/images/profile.png"
+    //     height={144}
+    //     width={144}
+    //     alt=""
+    //   />
+    //   <div>찾을 수 없는 페이지 입니다.</div>
+    // </div>
     <div
       style={{
         display: "flex",
@@ -12,13 +30,6 @@ const Custom404 = () => {
         height: "80vh",
       }}
     >
-      <Image
-        priority
-        src="/images/profile.png"
-        height={144}
-        width={144}
-        alt=""
-      />
       <div>찾을 수 없는 페이지 입니다.</div>
     </div>
   );
