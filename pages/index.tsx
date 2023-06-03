4030877e2d893c4ef085cb59b99ae3d62bedc1e8
psc <whirled7@gmail.com>
2023-06-03 19:02:30 +0900

    feat : post

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,31 @@
-import Container from "@/components/container";
+import Container from "components/container";
 import Image from "next/image";
 import React from "react";
 import metadata from "./data/metadata";
-import RecentPosts from "@/components/RecentPosts";
+import RecentPosts from "components/RecentPosts";
+import { InferGetStaticPropsType } from "next";
+import { allPosts } from "contentlayer/generated";
 
-const Home = () => {
+const Home = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <Container>
       <div className={`my-5 w-full`}>
-        <div className={`relative`}>
-          <Image
-            src={`/home.jpg`}
-            alt="대표 이미지"
-            width={300}
-            height={45}
-            layout={`responsive`}
-            objectFit="cover"
-            className={`rounded-3xl`}
-          />
-          <span
-            className={`absolute top-12 font-extrabold italic text-white text-5xl md:text-9xl text flex justify-center w-full drop-shadow-lg`}
-          >
-            {metadata.title}
-          </span>
-        </div>
-        <RecentPosts />
+        ...
+        <RecentPosts posts={posts} />
       </div>
     </Container>
   );
+};
+
+export const getStaticProps = async () => {
+  const posts = allPosts.sort(
+    (a, b) => Number(new Date(b.date)) - Number(new Date(a.date))
+  );
+  return {
+    props: {
+      posts,
+    },
+  };
 };
 
 export default Home;
